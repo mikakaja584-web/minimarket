2bd2f10b41416e2ad196025bafb2e95f363426fa
mikakaja584-web <mikakaja584@gmail.com>
2026-09-04 17:21:13 +0700

    update minimarket

---
+++ app/Console/Commands/CheckShopStatus.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CheckShopStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     */
+    protected $signature = 'pos:status {jam?}';
+
+    /**
+     * The console command description.
+     */
+    protected $description = 'Mengecek status operasional Toko Kelontong POS';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $jam = $this->argument('jam') ?? 10;
+
+        $namaKasir = $this->ask('Masukkan nama Anda: ');
+
+        $this->info('=== STATUS TOKO KELONTONG POS ===');
+
+        if ($jam >= 8 && $jam <= 21) {
+            $this->info("Halo {$namaKasir}, Status Toko pada jam {$jam}:00 WIB adalah: BUKA");
+            $this->comment('Silakan kasir bersiap di meja transaksi.');
+        } else {
+            $this->error("Halo {$namaKasir}, Status Toko pada jam {$jam}:00 WIB adalah: TUTUP");
+            $this->comment('Akses transaksi kasir dinonaktifkan sementara.');
+        }
+
+        return Command::SUCCESS;
+    }
+}
